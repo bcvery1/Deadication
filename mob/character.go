c4dfa9018dc068240cac8e59884c897bc0db7757
Ben Cragg <bcvery1@gmail.com>
2018-04-21 04:02:15 +0100

    Added character utility functions

--- mob/character.go
+++ mob/character.go
@@ -12,14 +12,50 @@ import (
 const (
   characterImagePath string = "assets/zombie.png"
   charScale float64 = 0.5
+  maxHealth int = 100
+  maxHunger int = 100
+  // How much HP is lost while starving
+  starvingHP int = 2
 )
 
 type CharacterMob struct {
   Mob
+  Health int
+  Hunger int
 }
 
 func (c CharacterMob) Update(win *pixelgl.Window, camPos pixel.Vec) {
   c.Sprites[c.State].Draw(win, pixel.IM.Moved(camPos).Scaled(camPos, charScale))
+}
+
+// Increase hunger
+func (c *CharacterMob) Eat(points int) {
+  c.Hunger += points
+  if c.Hunger > maxHunger {
+    c.Hunger = maxHunger
+  }
+}
+
+func (c *CharacterMob) GetHungry(points int) {
+  c.Hunger -= points
+  if c.Hunger <= 0 {
+    // Player is starving
+    c.Hurt(starvingHP)
+  }
+}
+
+// Injures the player
+// Returns if the player is dead
+func (c *CharacterMob) Hurt(hp int) bool {
+  c.Health -= hp
+  return c.Health <= 0
+}
+
+func (c *CharacterMob) Heal(hp int) {
+  c.Health += hp
+  if c.Health > maxHealth {
+    c.Health = maxHealth
+  }
 }
 
 // Loads the main characters sprite
