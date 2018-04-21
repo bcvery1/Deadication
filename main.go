f9b34312bc39092fff71d7fdfc746cce3a5990f3
Ben Cragg <bcvery1@gmail.com>
2018-04-21 03:02:46 +0100

    Drawing main character

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
   "log"
-  "time"
+  // "time"
+
+  "Deadication/mob"
 
   "github.com/faiface/pixel"
   "github.com/faiface/pixel/pixelgl"
+
+  "golang.org/x/image/colornames"
 )
 
 const (
-  winWidth float64 = 1024
+  winWidth float64  = 1024
   winHeight float64 = 768
 )
 
@@ -25,12 +29,20 @@ func run() {
     log.Fatal(err)
   }
   win.SetSmooth(true)
+  win.Clear(colornames.White)
 
-  last := time.Now()
+  characterSprite, err := mob.GetCharacterSprite()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // last := time.Now()
   for !win.Closed() {
-    dt := time.Since(last).Seconds()
-    last = time.Now()
-    
+    // dt := time.Since(last).Seconds()
+    // last = time.Now()
+
+    characterSprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+
     win.Update()
   }
 }
