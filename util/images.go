762b4215ecbb2204eee6b6f15677dcbec6e66cf4
Ben Cragg <bcvery1@gmail.com>
2018-04-21 21:12:40 +0100

    Resolved collision!!!

--- util/images.go
+++ util/images.go
@@ -10,6 +10,10 @@ import (
   "github.com/faiface/pixel"
 )
 
+var (
+  shiftDrawingBy pixel.Vec = pixel.Vec{16.0, 0.0}
+)
+
 type StaticObject struct {
   Sprite    *pixel.Sprite
   PosV      pixel.Vec
@@ -17,7 +21,7 @@ type StaticObject struct {
 }
 
 func (o *StaticObject) Draw(t pixel.Target) {
-  o.Sprite.Draw(t, pixel.IM.Moved(o.PosV))
+  o.Sprite.Draw(t, pixel.IM.Moved(o.PosV.Add(shiftDrawingBy)).Scaled(o.PosV, 2.0))
 }
 
 func NewStaticObject(spr *pixel.Sprite, pos pixel.Vec, canCollide bool) *StaticObject {
